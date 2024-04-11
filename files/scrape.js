3cfda7797fadc2f740341d495350c67d2ca70405
Taariq Elliott <taariq.elliott@yahoo.com>
2024-04-11 03:50:52 -0400

    Fixed JSON for entries without extensions

--- files/scrape.js
+++ files/scrape.js
@@ -7,28 +7,38 @@ async function scrapeAndSaveArtworks() {
   await page.goto("http://127.0.0.1:5500/files/van-gogh-paintings.html");
 
   const artworks = await page.$$eval(
-    "#_c2yRXMvVOs3N-QazgILgAg93 .MiPcId.klitem-tr.mlo-c > a",
+    "#_c2yRXMvVOs3N-QazgILgAg93 .MiPcId.klitem-tr.mlo-c a",
     (elements) =>
-      elements.map((e) => ({
-        name: e.querySelector(".kltat").innerText,
-        extensions: Array.from(
-          e.querySelectorAll(".ellip.klmeta"),
-          (ext) => ext.innerText
-        ),
-        link: `https://www.google.com${e.getAttribute("href")}`,
-        image:
+      elements.map((e) => {
+        const name = e.querySelector(".kltat").innerText;
+        const extensions = e.querySelectorAll(".ellip.klmeta");
+        const link = `https://www.google.com${e.getAttribute("href")}`;
+        const image =
           e.querySelector(".rISBZc.M4dUYb").getAttribute("src") !== undefined
             ? e.querySelector(".rISBZc.M4dUYb").getAttribute("src")
-            : null,
-      }))
+            : null;
+
+        // Construct the object with the desired order of properties
+        const artworkObj = {
+          name,
+          // Conditionally add extensions property if extensions exist
+          ...(extensions.length > 0 && {
+            extensions: Array.from(extensions, (ext) => ext.innerText),
+          }),
+          link,
+          image,
+        };
+
+        return artworkObj;
+      })
   );
 
   const artworkData = { artworks };
-  const savedFileName = "vangogh-taariq-elliott.json";
+  const fileName = "vangogh-taariq-elliott.json";
 
-  fileSystem.writeFile(savedFileName, JSON.stringify(artworkData), (err) => {
+  fileSystem.writeFile(fileName, JSON.stringify(artworkData), (err) => {
     if (err) throw err;
-    console.log(`Saved file ${savedFileName} successfully!`);
+    console.log(`Saved file ${fileName} successfully!`);
   });
 
   await puppeteerBrowser.close();
